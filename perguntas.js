f72b6352b0283a80030a51c98793911f71b053d8
camis113 <camila.mazzo.brito@escola.pr.gov.br>
2025-05-14 12:11:21 -0300

    14/05

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Ciências',
+    'Por que as árvores são verde?',
+    'Elas têm clorofila, uma substância que ajuda a planta a produzir alimento e que é verde.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é o maior continente?',
+    'O maior continente é a Ásia'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Quimica',
+    'Qual é o simbolo quimico da agua ?',
+    'H2O'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Me fale uma cor em Inglês?',
+    'Yellow (amarelo)'
 )
